5d7c9550d22a3009402f493935991bd1d3e848d8
João Marcelo Dantas <jmgrd98@gmail.com>
2023-03-16 15:13:36 -0300

    socks image

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
+  <div>
   <HeaderComponent/>
+
+  <main>
+  <h1>{{product}}</h1>
+
+  <div class="product-summary">
+    <img v-bind:src="image"/>
+
+    <p>Lorem</p>
+
+  </div>
+  </main>
+
+  </div>
 </template>
 
 <script>
@@ -10,6 +24,13 @@ export default {
   name: 'App',
   components: {
     HeaderComponent
+  },
+  data(){
+    return {
+      product: 'Socks',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMQAM-2DmaQkQAk4CHG_KagUFvO0j0JGAi0IiXKI_0oAe9TVZGnmp1DwmrYWeBUwdJZGU',
+
+    }
   }
 }
 </script>
@@ -22,5 +43,27 @@ export default {
   list-style: none;
   font-family: "Roboto Thin", sans-serif;
 }
+
+  main{
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+  }
+
+  .product-summary{
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 50%;
+  }
+
+    img{
+      border: 1px solid gray;
+      padding: 10px;
+    }
+
 
 </style>
